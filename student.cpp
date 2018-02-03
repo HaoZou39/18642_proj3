4c1961f0bf25442b94e79568a851094f4cdc6e65
davisdarius95 <dariusd@andrew.cmu.edu>
2018-02-02 20:46:34 -0500

    Final working version

--- student.cpp
+++ student.cpp
@@ -29,24 +29,21 @@ typedef enum {
     down,
     left,
     noName
-}
-direction;
+}direction;
 
 typedef enum {
     state0,
     state1,
     state2,
     state5 = 5
-}
-state;
+}state;
 
 typedef struct coordinates {
     int xa = 0;
     int ya = 0;
     int xb = 0;
     int yb = 0;
-}
-coo;
+}coo;
 
 typedef struct randosm {
     bool z;
@@ -56,8 +53,7 @@ typedef struct randosm {
     int q;
     int countdown;
     int TIMEOUT = 17; // bigger number slows down simulation so you can see what's happening
-}
-ran;
+}ran;
 
 // this procedure takes one step in the maze and returns   true=done / false=not done
 
@@ -69,12 +65,9 @@ bool moveTurtle(QPointF & pos_) {
     static coo place;
     static ran rando;
 
-    //ROS_INFO("Turtle update Called  countdown=%i", rando.countdown);
-
     rando.m = true;
 
     if (rando.countdown == 0) {
-        //ROS_INFO("begin countdown loop" );
         place.xa = pos_.x();
         place.ya = pos_.y();
         place.xb = pos_.x();
@@ -82,264 +75,212 @@ bool moveTurtle(QPointF & pos_) {
 
         switch (d) {
         case right:
-            place.xb += 1;
-            //ROS_INFO("countdown loop right case" );
-            break;
+             place.xb += 1;
+             break;
         case up:
-            place.yb += 1;
-            //ROS_INFO("countdown loop up case" );
-            break;
+             place.yb += 1;
+             break;
         case left:
-            place.xb += 1;
-            place.yb += 1;
-            place.ya += 1;
-            //ROS_INFO("countdown loop left case" );
-            break;
+             place.xb += 1;
+             place.yb += 1;
+             place.ya += 1;
+             break;
         case down:
-            place.xb += 1;
-            place.yb += 1;
-            place.xa += 1;
-            //ROS_INFO("countdown loop down case" );
-            break;
+             place.xb += 1;
+             place.yb += 1;
+             place.xa += 1;
+             break;
         default:
-            break;
-            //ROS_ERROR
+             break;
+             ROS_ERROR("Movement error");
         }
 
         rando.b = bumped(place.xa, place.ya, place.xb, place.yb);
         rando.e = atend(pos_.x(), pos_.y());
 
-        //ROS_INFO("before switch bump = %d" , rando.b );
-        //ROS_INFO("before switch attend = %d" , rando.e );
-
         /*The if/else statements below make the turtle turn to the right of its relative orientation
-  and checks if it bumps into a wall. If it does bump into a wall, it turns the turtle to the
-  left of its initial orientation*/
-      //ROS_INFO("d value = %i", d );
+          and checks if it bumps into a wall. If it does bump into a wall, it turns the turtle to the
+          left of its initial orientation*/
 
         switch (d) {
         case up:
             switch(s){
               case state2:
-                d = right;
-                s = state1;
-                break;
+                   d = right;
+                   s = state1;
+                   break;
               case state0:
-                if (rando.b) {
-                  d = left;
-                  s = state0;
-                } else{ 
-                    s = state2;
-                }
-                break;
+                   if (rando.b) {
+                      d = left;
+                      s = state0;
+                   }else{ 
+                      s = state2;
+                   }
+                   break;
               case state1:
-                if (rando.b) {
-                  d = left;
-                  s = state0;
-                } else{ 
-                    s = state2;
-                }
-                break;
+                   if (rando.b) {
+                      d = left;
+                      s = state0;
+                   }else{ 
+                      s = state2;
+                   }
+                   break;
               case state5:
-                if (rando.b) {
-                  d = left;
-                  s = state0;
-                } else{ 
-                    s = state2;
-                }
-                break;
+                   if (rando.b) {
+                      d = left;
+                      s = state0;
+                   }else{ 
+                      s = state2;
+                   }
+                   break;
               default:
-                break; //ROS_ERROR
+                   ROS_ERROR("Up case: state error");
+                   break;
             }
-
-
-            ///ROS_INFO("Switch up State = %i  Switch up Orientation = %i", s, d );
             break;
 
         case left:
             switch(s){
               case state2:
-                d = up;
-                s = state1;
-                break;
+                   d = up;
+                   s = state1;
+                   break;
               case state0:
-                if (rando.b) {
-                  d = down;
-                  s = state0;
-                } else {
-                  s = state2;
-                }
-                break;
+                   if (rando.b) {
+                      d = down;
+                      s = state0;
+                   }else{
+                      s = state2;
+                   }
+                   break;
               case state1:
-                if (rando.b) {
-                  d = down;
-                  s = state0;
-                } else {
-                  s = state2;
-                }
-                break;
+                   if (rando.b) {
+                      d = down;
+                      s = state0;
+                   }else{
+                      s = state2;
+                   }
+                   break;
               case state5:
-                if (rando.b) {
-                  d = down;
-                  s = state0;
-                } else {
-                  s = state2;
-                }
-                break;
+                   if (rando.b) {
+                      d = down;
+                      s = state0;
+                   }else{
+                      s = state2;
+                   }
+                   break;
               default:
-                break; //ROS_ERROR
+                   ROS_ERROR("Left case: state error");
+                   break;
             }
-
-            ///ROS_INFO("Switch left State = %i  Switch left Orientation = %i", s, d );
             break;
 
         case down:
             switch(s){
               case state2:
-                d = left;
-                s = state1;
-                break;
+                   d = left;
+                   s = state1;
+                   break;
               case state0:
-                if (rando.b) {
-                  d = right;
-                  s = state0;
-                } else{
-                  s = state2;
-                }
-                break;
+                   if (rando.b) {
+                      d = right;
+                      s = state0;
+                   }else{
+                      s = state2;
+                   }
+                   break;
               case state1:
-                if (rando.b) {
-                  d = right;
-                  s = state0;
-                } else{
-                  s = state2;
-                }
-                break;
+                   if (rando.b) {
+                      d = right;
+                      s = state0;
+                   }else{
+                      s = state2;
+                   }
+                   break;
               case state5:
-                if (rando.b) {
-                  d = right;
-                  s = state0;
-                } else{
-                  s = state2;
-                }
-                break;
+                   if (rando.b) {
+                      d = right;
+                      s = state0;
+                   }else{
+                      s = state2;
+                   }
+                   break;
               default:
-                break; //ROS_ERROR
+                   ROS_ERROR("Down case: state error");
+                   break;
             }
-
-            ///ROS_INFO("Switch down State = %i  Switch down Orientation = %i", s, d );
             break;
 
         case right:
             switch(s){
               case state2:
-                d = down;
-                s = state1;
-                break;
+                   d = down;
+                   s = state1;
+                   break;
               case state0:
-                if (rando.b) {
-                  d = up;
-                  s = state0;
-                } else{
-                  s = state2;
-                }
-                break;
+                   if (rando.b) {
+                      d = up;
+                      s = state0;
+                   }else{
+                      s = state2;
+                   }
+                   break;
               case state1:
-                if (rando.b) {
-                  d = up;
-                  s = state0;
-                } else{
-                  s = state2;
-                }
-                break;
+                   if (rando.b) {
+                      d = up;
+                      s = state0;
+                   }else{
+                      s = state2;
+                   }
+                   break;
               case state5:
-                if (rando.b) {
-                  d = up;
-                  s = state0;
-                } else{
-                  s = state2;
-                }
-                break;
+                   if (rando.b) {
+                      d = up;
+                      s = state0;
+                   }else{
+                      s = state2;
+                   }
+                   break;
               default:
-                break; //ROS_ERROR
+                   ROS_ERROR("Right case: state error");
+                   break;
             }
-
-            //ROS_INFO("Switch right State = %i  Switch right Orientation = %i", s, d );
             break;
 
         default:
-              switch (s) {
-              case state5:
-                  d = left;
-                  s = state0;
-                  ROS_INFO("Outer switch state5" );
-                  break;
-              case state0:
-                  ROS_INFO("Outer switch state0" );
-                  break;
-              case state1:
-                  ROS_INFO("Outer switch state1" );
-                  break;
-              case state2:
-                  ROS_INFO("Outer switch state2" );
-                  break;
-              default:
-                  ROS_INFO("Outer switch default" );
-                  break; //ROS_ERROR
-              }
-              if (rando.b) {
-                  d = right;
-                  s = state1;
-                  ROS_INFO("Outer switch bump" );
-              } else {
-                  s = state2;
-                  ROS_INFO("Outer switch no bump state2" );
-              }
-            ROS_INFO("Default State = %i  Default 1 Orientation = %i", s, d );
-            break; //ROS_ERROR
+            ROS_ERROR("Directional error");
+            break;
         }
 
-
-        //ROS_INFO("Orientation=%i  STATE=%i", d, s);
         rando.z = s == 2;
         rando.m = true;
 
-        //ROS_INFO("rando.z = %d" , rando.z );
-        //ROS_INFO("rando.m = %d" , rando.m );
-        //ROS_INFO("rando.e = %d" , rando.e );
-
         if (rando.z == true && rando.e == false) {
-          //ROS_INFO("rando.z is true and rando.e is false" );
             switch (d) {
             case noName:
-                pos_.setY(pos_.y() + 0);
-                //ROS_INFO("z & e noName case" );
-                break;
+                 pos_.setY(pos_.y() + 0);
+                 break;
             case left:
-                pos_.setY(pos_.y() + 1);
-                //ROS_INFO("z & e left case" );
-                break;
+                 pos_.setY(pos_.y() + 1);
+                 break;
             case down:
-                pos_.setX(pos_.x() + 1);
-                //ROS_INFO("z & e down case" );
-                break;
+                 pos_.setX(pos_.x() + 1);
+                 break;
             case right:
-                pos_.setY(pos_.y() - 1);
-                //ROS_INFO("z & e right case" );
-                break;
+                 pos_.setY(pos_.y() - 1);
+                 break;
             case up:
-                pos_.setX(pos_.x() - 1);
-                //ROS_INFO("z & e up case" );
-                break;
+                 pos_.setX(pos_.x() - 1);
+                 break;
             default:
-                //ROS_INFO("z & e default case" );
-                break; //ROS_ERROR
+                 ROS_ERROR("z & e: directional error");
+                 break;
             }
             rando.z = true;
             rando.m = false;
         }
-    
     }
+
     if (rando.countdown == 0) rando.countdown = rando.TIMEOUT;
     else rando.countdown -= 1;
 
